805b393b5e4b0bf06fe7de674aa16582746e7778
josh groft <joshgroft@iglou.com>
2026-04-03 21:14:46 -0400

    Initial commit
     Changes to be committed:
    	new file:   reverse-a-string.py

---
+++ reverse-a-string.py
@@ -0,0 +1,10 @@
+# i was trying to paste the task instructions here, but TopHat really wants me
+# to highlight or add a note when i select text in the browser so....
+
+# trying to reverse a string. for data analysis reasons.
+
+def reverse_string(txt):
+    return txt[::-1]
+
+print(reverse_string("hello")) # will output: olleh
+print(reverse_string("Python")) # will output: nohtyP
