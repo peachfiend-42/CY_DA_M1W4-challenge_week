9850662b68ac6a5253463f8e25a16f68af6a445f
josh groft <peachfiend@mx.sdf.org>
2026-04-06 16:37:29 -0400

    no wonder Brutus stabbed the guy. this sucked. 	modified:   caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,19 +1,29 @@
+# what a pain in the ass.
+
 def caesar_cipher(plain_txt, shift_step): # define func: take plain text and value to shift cipher
+
     encypted_txt = "" #establish variable to hold the encrypted text
-    for char in plain_txt: # as it says: for each character in the text we're encryping, do...
-        if char.isalpha(): # check to see if the character being encrypted is a letter
-            abc_fixer = ord(char) + shift_step # definte variable to apply the cipher shift
-            if abc_fixer > ord('z'): # if the shift goes beyond z...
-                abc_fixer -= 26 # ... then basically push it around to A and onward to get the right cipher letter
+
+    for char in plain_txt: # as it says: for each character in the text we're encryping...
+        if char.isalpha(): # ...check to see if the character being encrypted is a letter.
+
+            if char.isupper(): # because referencing both uppercase and lowercase letters in strings.
+                abc_fixer = ord(char) + shift_step # change the letter to an ascii code (int), apply cipher shift, store in abc_fixer.
+                if abc_fixer > ord('Z'): # if the shift goes beyond Z...
+                    abc_fixer -= 26 # ...then basically push it around to A and onward to get the right cipher letter.
+
+            elif char.islower(): # because referencing both uppercase and lowercase letters in strings.
+                abc_fixer = ord(char) + shift_step # change the letter to an ascii code (int), apply cipher shift, store in abc_fixer.
+                if abc_fixer > ord('z'): # if the shift goes beyond 'z'...
+                    abc_fixer -= 26 # ...then basically push it around to 'a' and onward to get the right cipher letter.
 
         else: # in case character is not a letter
-            abc_fixer = char # feels weird based on variable name, but if it's not a letter, leave it be.
+            abc_fixer = ord(char) # feels weird based on variable name, but if it's not a letter, leave it be.
 
-        latest_char = chr(abc_fixer)
-        encypted_txt += latest_char
-    print(encypted_txt)
-    return encypted_txt
-
+        latest_char = chr(abc_fixer) # turns ascii(unicode?) value of abc_fixer back to a letter
+        encypted_txt += latest_char # tack the latest encrypted letter/character onto the encrypted text we're building
+    print(encypted_txt) # does what it says
+    return encypted_txt # because functions must "return"?
 
 caesar_cipher("abc", 3) #Output:  "def"  
 caesar_cipher("xyz", 2) #Output:  "zab"  
